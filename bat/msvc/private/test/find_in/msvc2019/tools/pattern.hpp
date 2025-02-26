ce78fc1f2d42b8f7468c64d81081a93e63e98088
Kartonagnick <Kartonagnick@gmail.com>
2025-02-27 02:10:42 +0300

    upd: bat/msvc/private/test/find_in/msvc2019/tools/pattern.hpp (004) PRE

--- bat/msvc/private/test/find_in/msvc2019/tools/pattern.hpp
+++ bat/msvc/private/test/find_in/msvc2019/tools/pattern.hpp
@@ -1,4 +1,14 @@
 
+// --- workspace/scripts/bat/msvc                         [find_in][pattern.hpp]
+// encode of this source file must be 1251
+// reconstruct:
+//   [2024-02m-09][02:58:33] 004 Kartonagnick PRE
+//   [2023-06m-01][01:54:31] 003 Kartonagnick PRE
+//   [2022-08m-05][05:59:37] 001 Kartonagnick PRE
+
+//==============================================================================
+//==============================================================================
+
 #pragma once
 
 #ifndef dTOOLS_MATCH_PATTERN_USED_ 
@@ -15,12 +25,6 @@ namespace tools
         bool match_pattern(const s1& text, const s2& pattern) noexcept;
 
 } // namespace tools
-
-//==============================================================================
-//==============================================================================
-
-
-
 
 //==============================================================================
 //=== match_pattern(text, pattern) =============================================
@@ -52,8 +56,7 @@ namespace tools
         #undef dMP_POINTER
         #undef dMP_OTHER
 
-        template<class ch>
-        bool match_pattern(const ch* s, const ch* p) noexcept
+        template<class ch> bool match_pattern(const ch* s, const ch* p) noexcept
         {
             assert(s);
             assert(p);
@@ -77,6 +80,42 @@ namespace tools
                     return false;
         }
 
+        template<class ch>
+        constexpr ch up(const ch& symbol) noexcept
+        {
+            constexpr auto offset_eng = 'a' - 'A';
+            constexpr auto offset_rus = 'ÿ' - 'ß';
+
+            if (symbol >= 'a' && symbol <= 'z')
+                return static_cast<ch>(symbol - offset_eng);
+            if (symbol >= 'à' && symbol <= 'ÿ')
+                return static_cast<ch>(symbol - offset_rus);
+            return symbol;
+        }
+
+        template<class ch> bool match_pattern_insensitive(const ch* s, const ch* p) noexcept
+        {
+            assert(s);
+            assert(p);
+            const ch* rs = nullptr;
+            const ch* rp = nullptr;
+            while (true)
+                if (*p == '*')
+                    rs = s,
+                    rp = ++p;
+                else if (!*s)
+                    return !*p;
+                else if (up(*s) == up(*p) || *p == '?')
+                    ++s, 
+                    ++p;
+                else if (rs)
+                    s = ++rs, 
+                    p = rp;
+                else
+                    return false;
+        }
+
+
     } // namespace match_detail
 
     template<class s1, class s2>
@@ -89,11 +128,22 @@ namespace tools
         );
     }
 
+    template<class s1, class s2>
+    bool match_pattern_insensitive(const s1& text, const s2& pattern) noexcept
+    {
+        namespace detail = ::tools::match_detail;
+        return detail::match_pattern_insensitive(
+            detail::c_str(text), 
+            detail::c_str(pattern)
+        );
+    }
+
 } // namespace tools
 
 //==============================================================================
+//==============================================================================
 
-#endif // !dTOOLS_MATCH_PATTERN_USED_
+#endif // dTOOLS_MATCH_PATTERN_USED_
 
 
 
